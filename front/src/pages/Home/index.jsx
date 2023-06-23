650a5fa794bd39fec20d9a9b127990cf7eb42d88
Déborah <deborah.bude@gmail.com>
2023-06-23 15:40:57 +0200

    Gestion de l'utilisateur

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -3,8 +3,17 @@ import Feature from "../../components/Feature";
 import iconChat from "../../assets/icon-chat.png"
 import iconMoney from "../../assets/icon-money.png"
 import iconSecurity from "../../assets/icon-security.png"
+import {updateToken} from "../../redux";
+import {useDispatch} from "react-redux";
 
 export default function Home() {
+    const tokenStorage = localStorage.getItem('token')
+    const dispatch = useDispatch()
+    console.log(tokenStorage)
+    if (tokenStorage) {
+        dispatch(updateToken(tokenStorage))
+    }
+
     return (
         <main>
             <div className="hero">
--- front/src/pages/SignIn/index.jsx
+++ front/src/pages/SignIn/index.jsx
@@ -1,44 +1,62 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {userDataLog} from "../../fetchUser";
 import {useDispatch} from "react-redux";
 import {updateToken} from "../../redux";
 import {useNavigate} from "react-router-dom";
+import ErrorMessage from "../../components/ErrorMessage";
 
-export default function SignIn(dataUser) {
+export default function SignIn() {
     const [emailLog, setEmailLog] = useState();
     const [passwordLog, setPasswordLog] = useState();
     const [errorMessage, setErrorMessage] = useState(false)
+    const [remember, setRemember] = useState(false)
+    const [items, setItems] = useState([]);
 
     const dispatch = useDispatch()
     const navigate = useNavigate();
-    function handleSubmit (event) {
+
+    function handleSubmit(event) {
         event.preventDefault();
-        userDataLog(emailLog, passwordLog).then(function (resultatApi) {
-            if (resultatApi.status === 200) {
+
+        const form = event.target;
+        const formData = new FormData(form);
+
+        // Or you can work with it as a plain object:
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
+
+        setErrorMessage(false)
+        userDataLog(emailLog, passwordLog)
+            .then(function (resultatApi) {
                 dispatch(updateToken(resultatApi.body.token))
-                setErrorMessage(false)
+                if(remember) {
+                    localStorage.setItem('token', JSON.stringify(resultatApi.body.token))
+                }
                 navigate('/user', {replace: true});
-            } else {
+            })
+            .catch((error) => {
+                console.error(error)
                 setErrorMessage(true)
-            }
-        })
+            })
     }
 
-    return(
+    return (
         <main className="main bg-dark">
             <section className="sign-in-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign In</h1>
-                { errorMessage && <p>Erreur : L'utilisateur n'a pas été trouvé</p> }
+                {errorMessage && <ErrorMessage errorMessage="User not found in database"/>}
                 <form method="post" id="form-user-connect" onSubmit={handleSubmit}>
                     <div className="input-wrapper">
-                        <label htmlFor="username">Username</label><input type="text" id="username" onChange={e => setEmailLog(e.target.value)}/>
+                        <label htmlFor="username">Username</label><input type="text" name="username" id="username"
+                                                                         onChange={e => setEmailLog(e.target.value)}/>
                     </div>
                     <div className="input-wrapper">
-                        <label htmlFor="password">Password</label><input type="password" id="password" onChange={e => setPasswordLog(e.target.value)}/>
+                        <label htmlFor="password">Password</label><input type="password" name="password" id="password"
+                                                                         onChange={e => setPasswordLog(e.target.value)}/>
                     </div>
                     <div className="input-remember">
-                        <input type="checkbox" id="remember-me"/><label htmlFor="remember-me">Remember me</label>
+                        <input name="rememberme"  type="checkbox" id="remember-me" onChange={e => setRemember(e.target.checked) }/><label htmlFor="remember-me">Remember me</label>
                     </div>
                     <button className="sign-in-button">Sign In</button>
                 </form>
