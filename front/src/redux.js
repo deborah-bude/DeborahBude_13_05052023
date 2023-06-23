881a0d6f1104374bbec3c2550a4c85233304c90b
Déborah <deborah.bude@gmail.com>
2023-06-23 15:40:22 +0200

    Redux et Redux toolkit

--- front/src/redux.js
+++ front/src/redux.js
@@ -1,20 +1,41 @@
-import {configureStore, createSlice} from "@reduxjs/toolkit";
+import {configureStore, createSelector, createSlice} from "@reduxjs/toolkit";
 
 const tokenSlice = createSlice(({
-    name: "token",
+    name: 'token',
     initialState: null,
     reducers: {
         updateToken: (state, action) => {
-            const newToken = action.payload
-            state = newToken
+            return action.payload
         }
-    }
+    },
+
+}))
+
+const userSlice = createSlice(({
+    name: 'user',
+    initialState: {
+        lastName: null,
+        firstName: null
+    },
+    reducers: {
+        userName: (state, action) => {
+            return action.payload
+        },
+        editName: (state, action) => {
+            return action.payload
+        }
+    },
+
 }))
 
 export const { updateToken } = tokenSlice.actions;
+export const { userName, editName } = userSlice.actions;
+
+export const isUserAuthenticatedSelector = createSelector((state) => state, state => !!state.token)
 
 export const store = configureStore({
     reducer: {
-        token: tokenSlice.reducer
+        token: tokenSlice.reducer,
+        userName: userSlice.reducer,
     }
 })
